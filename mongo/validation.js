6fb4cb81d01faf17123909aba0bc9e89d21b8ede
paulapivat <paul.apivat@gmail.com>
2021-07-21 12:32:10 +0700

    update validation.js to validate bountyCard schema for primary key fields

--- mongo/validation.js
+++ mongo/validation.js
@@ -1,38 +1,59 @@
-db.createCollection("posts", {
+db.createCollection("bountyCard", {
   validator: {
     $jsonSchema: {
       bsonType: "object",
-      required: ["title", "text", "creator", "comments"],
+      required: [
+        "season",
+        "bountyTitle",
+        "bountyDescription",
+        "bountyCriteria",
+        "bountyReward",
+        "bountyCreatedBy",
+        "bountyCreatedAt",
+        "bountyDueAt",
+      ],
       properties: {
-        title: {
+        season: {
+          bsonType: "double",
+          description: "must be a double and is required",
+        },
+        bountyTitle: {
           bsonType: "string",
           description: "must be a string and is required",
         },
-        text: {
+        bountyDescription: {
           bsonType: "string",
-          description: "must be a string and is required",
+          description: "must be an string and is required",
         },
-        creator: {
+        bountyCriteria: {
+          bsonType: "string",
+          description: "must be an string and is required",
+        },
+        bountyReward: {
+          bsonType: "object",
+          description: "must be an object and is required",
+          properties: {
+            currency: {
+              bsonType: "string",
+              description: "must be a string and is required",
+            },
+            amount: {
+              bsonType: "double",
+              description: "must be a double and is required",
+            },
+          },
+        },
+        bountyCreatedBy: {
           bsonType: "objectId",
           description: "must be an objectId and is required",
         },
-        comments: {
-          bsonType: "array",
-          description: "must be an array and is required",
-          items: {
-            bsonType: "object",
-            required: ["text", "author"],
-            properties: {
-              test: {
-                bsonType: "string",
-                description: "must be a string and is required",
-              },
-              author: {
-                bsonType: "objectId",
-                description: "must be an objectId and is required",
-              },
-            },
-          },
+        bountyCreatedAt: {
+          bsonType: "date",
+          description: "must be a date and is required",
+        },
+        bountyDueAt: {
+          bsonType: "date",
+          description: "must be a date and is required",
         },
       },
     },
