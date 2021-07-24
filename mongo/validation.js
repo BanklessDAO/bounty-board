727b0caf3e0591239182b39e674d03d36d6f7168
paulapivat <paul.apivat@gmail.com>
2021-07-24 10:06:12 +0700

    remove bounty from key fields of bboard_v3.json and validation.js

--- mongo/validation.js
+++ mongo/validation.js
@@ -4,32 +4,32 @@ db.createCollection("bountyCard", {
       bsonType: "object",
       required: [
         "season",
-        "bountyTitle",
-        "bountyDescription",
-        "bountyCriteria",
-        "bountyReward",
-        "bountyCreatedBy",
-        "bountyCreatedAt",
-        "bountyDueAt",
+        "Title",
+        "Description",
+        "Criteria",
+        "Reward",
+        "CreatedBy",
+        "CreatedAt",
+        "DueAt",
       ],
       properties: {
         season: {
           bsonType: "double",
           description: "must be a double and is required",
         },
-        bountyTitle: {
+        Title: {
           bsonType: "string",
           description: "must be a string and is required",
         },
-        bountyDescription: {
+        Description: {
           bsonType: "string",
           description: "must be an string and is required",
         },
-        bountyCriteria: {
+        Criteria: {
           bsonType: "string",
           description: "must be an string and is required",
         },
-        bountyReward: {
+        Reward: {
           bsonType: "object",
           description: "must be an object and is required",
           properties: {
@@ -43,15 +43,15 @@ db.createCollection("bountyCard", {
             },
           },
         },
-        bountyCreatedBy: {
+        CreatedBy: {
           bsonType: "objectId",
           description: "must be an objectId and is required",
         },
-        bountyCreatedAt: {
+        CreatedAt: {
           bsonType: "date",
           description: "must be a date and is required",
         },
-        bountyDueAt: {
+        DueAt: {
           bsonType: "date",
           description: "must be a date and is required",
         },
