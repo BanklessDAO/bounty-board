e674fb0e35d44f47224b51f6c599a5c6ed6c9580
Oktalize <oxta@protonmail.com>
2021-08-02 21:42:14 -0500

    First attempt at single bounty filter

--- packages/react-app/src/components/pages/Bounties/index.tsx
+++ packages/react-app/src/components/pages/Bounties/index.tsx
@@ -3,13 +3,21 @@ import Filters from './Filters'
 import BountyList from './BountyList'
 import useSWR from 'swr'
 
+export type FilterProps = {
+  id?: string | string[]
+}
+
 const fetcher = (url: string) =>
   fetch(url)
     .then((res) => res.json())
     .then((json) => json.data)
 
-const Bounties = (): JSX.Element => {
-  const { data: bounties, error } = useSWR(`/api/bounties`, fetcher)
+const Bounties = ({ id }: FilterProps): JSX.Element => {
+  /* Bounties will fetch all data to start, unless prefiltering is set */
+  const { data: bounties, error } = useSWR(
+    id ? `/api/bounties/${id}` : `/api/bounties`,
+    fetcher
+  )
 
   if (error) return <p>Failed to load</p>
   if (!bounties) return <p>Loading...</p>
@@ -23,7 +31,11 @@ const Bounties = (): JSX.Element => {
       gridGap="4"
     >
       <Filters />
-      <BountyList bounties={bounties} />
+      {id ? (
+        <BountyList bounties={[bounties]} />
+      ) : (
+        <BountyList bounties={bounties} />
+      )}
     </Stack>
   )
 }
--- packages/react-app/src/pages/[id]/index.tsx
+++ packages/react-app/src/pages/[id]/index.tsx
@@ -1,94 +1,15 @@
-import { useState } from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
-import dbConnect from '../../utils/dbConnect'
-import Bounty from '../../models/Bounty'
-import { BountyBoardProps } from '../../models/Bounty'
+import Layout from '../../components/global/SiteLayout'
+import Bounties from '../../components/pages/Bounties'
 
-/* Allows you to view bounty card info and delete bounty card*/
-const BountyPage = ({
-  _id,
-  status,
-  title,
-  season,
-  createdBy,
-  reward,
-  statusHistory,
-}: BountyBoardProps): JSX.Element => {
+const BountyPage = (): JSX.Element => {
   const router = useRouter()
-  const [message, setMessage] = useState('')
-  const handleDelete = async () => {
-    if (status.match(/^draft/i)) {
-      return <p>Bounty is no longer a draft. Unauthorized to delete.</p>
-    }
-    const bountyID = router.query.id
-
-    try {
-      await fetch(`/api/bounties/${bountyID}`, {
-        method: 'Delete',
-      })
-      router.push('/')
-    } catch (error) {
-      setMessage('Failed to delete the bounty.')
-    }
-  }
-
+  const { id } = router.query
   return (
-    <div key={_id}>
-      <div className="card">
-        <h5 className="bountyTitle">{title}</h5>
-        <div className="main-content">
-          <p className="bountyTitle">Bounty Name: {title}</p>
-          <p className="bountySeason">Season: {season}</p>
-          <p className="bountyCreatedBy">
-            Created By: {createdBy.discordHandle}
-          </p>
-          <p className="bountyStatus">Status: {status}</p>
-
-          {/* Extra Bounty Info: Reward and Guild */}
-          <div className="bountyReward">
-            <p className="label">Bounty Reward</p>
-            <ul>
-              <li>Amount: {reward.amount}</li>
-              <li>Currency: {reward.currency}</li>
-            </ul>
-          </div>
-          <div className="bounty status history">
-            <p className="label">Bounty Status History</p>
-            <ul>
-              {statusHistory &&
-                statusHistory.map((record: any) => (
-                  <li key={record.setAt}>
-                    {record.status} {record.setAt}{' '}
-                  </li>
-                ))}
-            </ul>
-          </div>
-
-          <div className="btn-container">
-            <Link href="/[id]/edit" as={`/${_id}/edit`}>
-              <button className="btn edit">Edit</button>
-            </Link>
-            <button className="btn delete" onClick={handleDelete}>
-              Delete
-            </button>
-          </div>
-        </div>
-      </div>
-      {message && <p>{message}</p>}
-    </div>
+    <Layout title="DAO Bounty Board">
+      <Bounties id={id} />
+    </Layout>
   )
-}
-
-export async function getServerSideProps({ params }: { params: any }): Promise<{
-  props: BountyBoardProps
-}> {
-  await dbConnect()
-
-  const bounty = await Bounty.findById(params.id).lean()
-  bounty._id = bounty._id.toString()
-
-  return { props: bounty }
 }
 
 export default BountyPage
