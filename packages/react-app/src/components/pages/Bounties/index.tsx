e99c80eed55d095154a9497e9f41658c00a08aa7
Behold <Behold.3th@gmail.com>
2021-08-12 02:28:13 -0400

    Implemented scrappy client side pagination. Button logic needs updating, as does button styling

--- packages/react-app/src/components/pages/Bounties/index.tsx
+++ packages/react-app/src/components/pages/Bounties/index.tsx
@@ -3,26 +3,53 @@ import Filters from './Filters'
 import BountyAccordion from './BountyAccordion'
 import useSWR from 'swr'
 import { BountyCard } from './Bounty'
+import React, { useState } from 'react'
+import Bounty from '../../../models/Bounty'
 
 export type PreFilterProps = {
   id?: string | string[]
 }
+
+export const PAGE_SIZE = 10
 
 const fetcher = (url: string) =>
   fetch(url)
     .then((res) => res.json())
     .then((json) => json.data)
 
+
 const Bounties = ({ id }: PreFilterProps): JSX.Element => {
+  const [page, setPage] = useState(0)
+  return (
+    <>
+      <PaginatedBounties id={id} page={page}/>
+      <button onClick={() => setPage(page + 1)}>Next Page </button><br />
+      <button onClick={() => setPage(Math.min(page - 1,0))}>Previous Page </button>
+    </>
+  )
+}
+
+
+const PaginatedBounties = (props: any): JSX.Element => {
   /* Bounties will fetch all data to start, unless a single bounty is requested */
+  
   const { data: bounties, error } = useSWR(
-    id ? `/api/bounties/${id}` : `/api/bounties`,
+    props.id ? `/api/bounties/${props.id}` : `/api/bounties`,
     fetcher
   )
 
-  if (error) return <p>Failed to load</p>
-  if (!bounties) return <p>Loading...</p>
+  console.log(props.page)
 
+  if (error) return <p>Failed to load</p>
+  if (!bounties) return <p>Loading...</p> 
+  
+  let correctedPage = props.page
+  if (PAGE_SIZE*props.page > bounties.length) correctedPage=Math.floor(bounties.length/PAGE_SIZE)
+  else if (props.page < 0) correctedPage = 0
+
+  const paginatedBounties = bounties.slice(
+    PAGE_SIZE*correctedPage, Math.min(bounties.length, PAGE_SIZE*(correctedPage+1)))
+      
   return (
     <Stack
       direction={{ base: 'column', lg: 'row' }}
@@ -31,13 +58,14 @@ const Bounties = ({ id }: PreFilterProps): JSX.Element => {
       fontWeight="600"
       gridGap="4"
     >
-      {id ? (
+      {props.id ? (
         <BountyCard {...bounties} />
       ) : (
         <>
           <Filters />
-          <BountyAccordion bounties={bounties} />
+          <BountyAccordion bounties={paginatedBounties} />
         </>
+        
       )}
     </Stack>
   )
