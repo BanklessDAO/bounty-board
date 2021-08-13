0da3efb53e7baf8eb8d150f1f91b17ece36f7c09
Behold3th <85538143+Behold3th@users.noreply.github.com>
2021-08-12 22:50:14 -0400

    Feature/53 Pagination (#59)
    * Implemented scrappy client side pagination. Button logic needs updating, as does button styling
    
    * feature/53: fixed buggy next page and previous page logic. Implemented an API route to get # bounties in the collection
    
    * feature/53: refactored onClick methods. Attempted to wire in Chakra, couldn't get onClick to fire.
    
    * feature/53: implemented chakra styling over html buttons w Zberwaldt !
    
    * feature/53 hand merged in Oktal's refactoring of paginated bounties to make it a child component of Bounties and the use of Button rather than chakra.button. refactored math

--- packages/react-app/src/components/pages/Bounties/index.tsx
+++ packages/react-app/src/components/pages/Bounties/index.tsx
@@ -1,12 +1,15 @@
-import { Stack } from '@chakra-ui/react'
+import { Button, Stack } from '@chakra-ui/react'
 import Filters from './Filters'
 import BountyAccordion from './BountyAccordion'
 import useSWR from 'swr'
 import { BountyCard } from './Bounty'
+import React, { useState } from 'react'
 
 export type PreFilterProps = {
   id?: string | string[]
 }
+
+export const PAGE_SIZE = 10
 
 const fetcher = (url: string) =>
   fetch(url)
@@ -15,6 +18,20 @@ const fetcher = (url: string) =>
 
 const Bounties = ({ id }: PreFilterProps): JSX.Element => {
   /* Bounties will fetch all data to start, unless a single bounty is requested */
+  const [page, setPage] = useState(0)
+
+  const incrementPage = () => {
+    const numFullPages = Math.floor(bounties.length / PAGE_SIZE)
+    const hasExtraPage = bounties.length % PAGE_SIZE != 0
+    const maxPages = hasExtraPage ? numFullPages + 1 : numFullPages
+
+    setPage(Math.min(page + 1, maxPages - 1)) //pages are 0 indexed
+  }
+
+  const decrementPage = () => {
+    setPage(Math.max(page - 1, 0))
+  }
+
   const { data: bounties, error } = useSWR(
     id ? `/api/bounties/${id}` : `/api/bounties`,
     fetcher
@@ -23,7 +40,13 @@ const Bounties = ({ id }: PreFilterProps): JSX.Element => {
   if (error) return <p>Failed to load</p>
   if (!bounties) return <p>Loading...</p>
 
+  const paginatedBounties = bounties.slice(
+    PAGE_SIZE * page,
+    Math.min(bounties.length, PAGE_SIZE * (page + 1))
+  )
+
   return (
+    <>
     <Stack
       direction={{ base: 'column', lg: 'row' }}
       align="top"
@@ -36,10 +59,19 @@ const Bounties = ({ id }: PreFilterProps): JSX.Element => {
       ) : (
         <>
           <Filters />
-          <BountyAccordion bounties={bounties} />
+          <BountyAccordion bounties={paginatedBounties} />
         </>
       )}
     </Stack>
+    <Stack spacing={2} direction="row">
+      <Button size="sm" colorScheme="teal" onClick={decrementPage}>
+        Previous Page
+      </Button>
+      <Button size="sm" colorScheme="teal" onClick={incrementPage}>
+        Next Page
+      </Button>
+    </Stack>
+  </>
   )
 }
 
