7e2823231621833691e67c36214d30bc323e13a3
Behold <Behold.3th@gmail.com>
2021-11-04 02:30:17 -0400

    feature/95_discord-oauth: WIP callback functionality to fetch guilds from api/user/@me

--- packages/react-app/src/pages/api/auth/[...nextauth].js
+++ packages/react-app/src/pages/api/auth/[...nextauth].js
@@ -1,3 +1,4 @@
+import { profile } from 'console';
 import NextAuth from 'next-auth';
 import DiscordProvider from 'next-auth/providers/discord';
 
@@ -10,4 +11,37 @@ export default NextAuth({
 			authorization: 'https://discord.com/api/oauth2/authorize?scope=identify+guilds',
 		}),
 	],
+	callbacks: {
+		async jwt({token, user, account, profile}) {
+			// Add auth_time to token on signin in
+
+			if(user) {
+			token.profile = profile;
+			token.accessToken = account.accessToken;
+			token.refreshToken = account.refreshToken;
+			}
+
+
+			console.log("jwt",{account, user})
+			return token;
+		},
+	// 	async session({ session, token, user }) {
+	// 	//   // Send properties to the client, like an access_token from a provider.
+
+	// 	//   session.user.accessToken = token.accessToken;
+	// 	//   session.user.refreshToken = token.refreshToken;
+
+	// 	//   session.user.profile = token.profile;
+	// 	//   console.log("session",{ session, token, user })
+	// 	//   const guildsfetch = await fetch('https://discord.com/api/users/@me', {
+	// 	// 	headers:  {
+	// 	// 		authorization: `Bearer ${token.accessToken}`,
+	// 	//   }
+	// 	// })
+	// 	// const guilds = await guildsfetch.json()
+	// 	// session.user.profile.guilds = guilds
+
+	// 	// return session
+	// 	// }
+	  },
 });
