4bb1ba6029b2dc89fcb8e062dd5bed8718b1d9fc
paulapivat <paul.apivat@gmail.com>
2021-12-10 08:14:33 +0700

    establish sentry connection, update sentry config files, update pages/api/customers/index.ts

--- packages/react-app/sentry.client.config.js
+++ packages/react-app/sentry.client.config.js
@@ -2,16 +2,20 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-	dsn: SENTRY_DSN || 'https://8cd5c6d0a0a14e2ab48371d22a9535b4@o1071534.ingest.sentry.io/6069034',
-	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 1.0,
-	// ...
-	// Note: if you want to override the automatic release value, do not set a
-	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
-	// that it will also get attached to your source maps
+  dsn:
+    SENTRY_DSN ||
+    "https://8cd5c6d0a0a14e2ab48371d22a9535b4@o1071534.ingest.sentry.io/6069034",
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  debug: true,
+  autoSessionTracking: true,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
 });
--- packages/react-app/src/pages/api/customers/index.ts
+++ packages/react-app/src/pages/api/customers/index.ts
@@ -6,12 +6,14 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ): Promise<void> {
-	await dbConnect();
+	const dbcheck = await dbConnect();
+	console.debug(dbcheck);
 
 	switch (req.method) {
 	case 'GET':
 		try {
 			const data = await service.getCustomers();
+			console.log(data);
 			res.status(200).json({ success: true, data });
 		} catch (error) {
 			res.status(400).json({ success: false });
