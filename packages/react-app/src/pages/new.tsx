7dc75bcc4abc2f9f7ecdc8693b845c4770ecfa04
jordaniza <jordan.imran@predixa.ai>
2022-01-21 13:05:23 +0400

    form data sent to preview

--- packages/react-app/src/pages/new.tsx
+++ packages/react-app/src/pages/new.tsx
@@ -1,4 +1,4 @@
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import React, { useEffect, useState } from 'react';
@@ -28,7 +28,10 @@ import {
   FormHelperText,
 	Textarea,
 } from '@chakra-ui/react';
-import Bounty, { BountySchema } from '@app/models/Bounty';
+import Bounty, { BountyCollection, BountySchema, Status } from '@app/models/Bounty';
+import { filterGuildsToCustomers } from '@app/services/customer.service';
+import { FaFileVideo } from 'react-icons/fa';
+import bountyStatus from '@app/constants/bountyStatus';
 
 const currencies = [
   'bank', 'eth', 'city'
@@ -37,18 +40,25 @@ const currencies = [
 const defaultValues = {
   title: 'Create a logo for the bountyboard',
   description: 'We need a sharp and stylish logo in the next couple of weeks',
-  reward: 1000,
+  reward: '1000',
   currency: 'BANK',
   criteria: 'PNG and SVG images submitted and approved by the team',
-  dueAt: new Date(),
-  gated: '#level-2',
+  dueAt: new Date().toDateString(),
+  gatedTo: '#level2',
   multiClaim: false,
   evergreen: false,
 } 
 
-const ExpansionField = ({ label, children }: { label: string, children: React.ReactNode }) => {
-  const [multi, setMulti] = useState(false);
-  const toggleMulti = () => setMulti(!multi);
+
+
+const ExpansionField = ({
+  label, children, value, onChange
+}: {
+  label: string,
+  children: React.ReactNode,
+  value: boolean,
+  onChange: (value: boolean) => void
+}) => {
   return (
     <Accordion 
     w="full"
@@ -57,7 +67,9 @@ const ExpansionField = ({ label, children }: { label: string, children: React.Re
     <AccordionItem
     >
       <AccordionButton 
-        onClick={() => toggleMulti()}
+        onClick={() => {
+          onChange(value = !value)
+        }}
         display="flex"
         justifyContent="space-between"
       >
@@ -66,9 +78,9 @@ const ExpansionField = ({ label, children }: { label: string, children: React.Re
       </FormLabel>
         <Box
           borderRadius="50%"
-          bg={multi ? "green": ""}
+          bg={value ? "green": ""}
           border="2px solid"
-          borderColor={multi ? "green" : "lightBlue"}
+          borderColor={value ? "green" : "lightBlue"}
           width="12px"
           height="12px"
         />
@@ -81,44 +93,90 @@ const ExpansionField = ({ label, children }: { label: string, children: React.Re
   )
 }
 
+const dateIsNotInPast = (d: string): string | boolean => {
+  const dt = new Date(d).getTime();
+  const today = new Date();
+  const todayAtMidnight = today.setHours(0,0,0,0);
+  const validDate = (dt - todayAtMidnight) >= 0;
+  return validDate ? true : 'Cannot set a date in the past'
+}
+
+const validNonNegativeDecimal = (v: string): string | boolean => {
+    if (!Number(v)) return 'Not a valid reward'
+    return Number(v) > 0 ? true : 'Must be > 0'
+}
+
 const required = 'This field is required';
 
-const BountyCreateSchema = yup.object().shape({
-  title: yup
-    .string()
-    .required('Please provide a title for this Bounty.')
-    .max(250, 'Title cannot be more than 250 characters'),
-  description: yup
-    .string()
-    .required('Please provide the bounty description')
-    .max(4000, 'Description cannot be more than 4000 characters'),
-  criteria: yup
-    .string()
-    .required('Please provide the bounty criteria')
-    .max(4000, 'Criteria cannot be more than 4000 characters'),
-  rewardAmount: yup
-    .number()
-    .typeError('Invalid number')
-    .positive()
-    .required('Please provide the bounty reward amount'),
-});
+const NotNeededFields = [
+  '_id',
+  'claimedBy', 
+  'submissionNotes',
+  'submissionUrl',
+  'season',
+  'claimedAt',
+  'submittedAt',
+  'reviewedAt',
+  'reviewedBy',
+  'submittedBy'
+] as const
+
+const generatePreviewData = (data: typeof defaultValues): Omit<BountyCollection, typeof NotNeededFields[number]> => {
+  const amount = Number(data.reward);
+  const decimalSplit = data.reward.split('.');
+  const hasDecimals = decimalSplit.length > 1
+  const amountWithoutScale = hasDecimals ? Number(decimalSplit.join('')) : amount;
+  const scale = hasDecimals ? decimalSplit[1].length : 0; 
+  return {  
+    title: data.title,
+    description: data.description,
+    criteria: data.criteria,
+    customer_id: '',
+    status: bountyStatus.DRAFT,
+    dueAt: data.dueAt,
+    reward: {
+      amount,
+      currency: data.currency.toUpperCase(),
+      amountWithoutScale,
+      scale,
+    },
+    editKey: '',
+    statusHistory: [
+      {
+        status: bountyStatus.DRAFT,
+        modifiedAt: new Date().toDateString()
+      }
+    ],
+    discordMessageId: '',
+    createdAt: new Date().toDateString(),
+    createdBy: {
+      discordHandle: '',
+      discordId: ''
+    }
+  }
+}
 
 const Form = () => {
   const formControlProps: FormControlProps = { mt: '5'};
-  const [dueAt, setDueAt] = useState<Date>(new Date());
+  const [gated, setGated] = useState(false);
   const {
     register,
     handleSubmit,
+    control,
     formState: {
       errors
     }
   } = useForm({
     defaultValues,
-    resolver: yupResolver(BountyCreateSchema)
   })
   return (
     <Layout title="Create a new Bounty">
-    <form onSubmit={handleSubmit(data => console.debug({ data }))}>
+    <form onSubmit={handleSubmit(data => {
+        let _data: Partial<typeof data>;
+        // console.debug({ data });
+        const preview = generatePreviewData(data);
+        console.debug({preview})
+      })}>
       <FormControl
         isInvalid={!!errors.title}
         {...formControlProps}
@@ -152,19 +210,31 @@ const Form = () => {
       </FormControl>
 
       <FormControl
+        isInvalid={!!errors.dueAt}
         {...formControlProps}
       >
         <FormLabel htmlFor="dueat"
           mr="0"
           >
           Due At
-          <DatePicker
-            selected={dueAt}
-            onChange={d => d && setDueAt(d)}
-            id="published-date"
-            showPopperArrow={true}
+          </FormLabel>
+          <Controller
+            name="dueAt"
+            control={control}
+            rules={{
+              required,
+              validate: v => dateIsNotInPast(v)
+            }}
+            render={({ field }) => 
+              <DatePicker
+                selected={new Date(field.value)}
+                onChange={d => field.onChange(d)}
+                id="published-date"
+                showPopperArrow={true}
+              />
+            }
           />
-        </FormLabel>
+          <FormErrorMessage>{errors.dueAt?.message}</FormErrorMessage>
       </FormControl>
 
       <Flex>
@@ -175,7 +245,7 @@ const Form = () => {
           >
           <FormLabel htmlFor='reward'>Reward</FormLabel>
           <Input id='reward' {...register('reward', {
-              required, min: 1
+              required, validate: (v: string) => validNonNegativeDecimal(v)
             })} />
           <FormErrorMessage>{errors.reward?.message}</FormErrorMessage>
         </FormControl>
@@ -196,78 +266,113 @@ const Form = () => {
         bg="rgba(0,0,0,0.2)"
         {...formControlProps}
       >
-        <ExpansionField label="Allow Multiple Claimants?">
-          <FormControl>
-            <Flex mb="3" alignItems="center">
-            <InfoIcon mr="3"/>
-                <Text
-                  p="0"
-                  m="0"
-                  fontSize="12"
-                  fontStyle="italic"
-                >
-                 Multiple People will be able to claim the bounty
-            </Text>
-          </Flex>            
-          </FormControl>
-        </ExpansionField>
+        <Controller
+          name="multiClaim"
+          control={control}   
+          render={({ field }) =>  
+            <ExpansionField
+              onChange={field.onChange}
+              value={field.value}
+              label="Allow Multiple Claimants?"
+              >
+              <FormControl>
+                <Flex mb="3" alignItems="center">
+                <InfoIcon mr="3"/>
+                    <Text
+                      p="0"
+                      m="0"
+                      fontSize="12"
+                      fontStyle="italic"
+                    >
+                     Multiple People will be able to claim the bounty
+                </Text>
+              </Flex>            
+              </FormControl>
+            </ExpansionField>
+          }
+        />
       </FormControl>
 
       <FormControl
         bg="rgba(0,0,0,0.2)"
         {...formControlProps}
       >
-        <ExpansionField label="Recurring Bounty?">
-            <FormLabel htmlFor='maxClaim'></FormLabel>
-              <Flex
-                justifyContent="space-evenly"
-              >
-                <InfoIcon mr="3" mt="1"/>
-                <Text
-                  p="0"
-                  m="0"
-                  fontSize="12"
-                  fontStyle="italic"
+         <Controller
+            name="evergreen"
+            control={control}   
+            render={({ field }) =>  
+              <ExpansionField 
+                value={field.value}
+                onChange={field.onChange}
+                label="Recurring Bounty?"
                 >
-                  A recurring bounty will automatically re-list once claimed
-                </Text>
-              </Flex>
-        </ExpansionField>
-        </FormControl>
+              <FormLabel htmlFor='maxClaim'></FormLabel>
+                <Flex
+                  justifyContent="space-evenly"
+                >
+                  <InfoIcon mr="3" mt="1"/>
+                  <Text
+                    p="0"
+                    m="0"
+                    fontSize="12"
+                    fontStyle="italic"
+                  >
+                    A recurring bounty will automatically re-list once claimed
+                  </Text>
+                </Flex>
+              </ExpansionField>
+            } />
+            </FormControl>
 
         <FormControl
-        bg="rgba(0,0,0,0.2)"
-        {...formControlProps}
-      >
-        <ExpansionField label="Restrict Claimants?">
-            <FormLabel htmlFor='gated'>
-              <Flex
-                justifyContent="space-evenly"
-                alignItems="start"
-                px="1"
-              >
-                <InfoIcon mr="3" mt="1"/>
-                <Text
-                  p="0"
-                  m="0"
-                  fontSize="12"
-                  fontStyle="italic"
-                  overflowWrap="break-word"
+          isInvalid={!!errors.gatedTo}
+          bg="rgba(0,0,0,0.2)"
+          {...formControlProps}
+        >
+              <ExpansionField
+                value={gated}
+                onChange={setGated} 
+                label="Restrict Claimants?"
                 >
-                  Restrict the bounty to allow claiming only by certain users or roles
-                </Text>
-              </Flex>
-              </FormLabel>
-              <Select id='gated' {...register('gated', { required })}>
-                {currencies.map(c => <option key={c}>{c.toUpperCase()}</option>)}
-              </Select>
-        </ExpansionField>        
-      </FormControl>
-      <Button
-        mt="5"
-        w="full" 
-        type="submit">Preview</Button>
-    </form>
+                  <FormLabel htmlFor='gatedTo'>
+                    <Flex
+                      justifyContent="space-evenly"
+                      alignItems="start"
+                      px="1"
+                    >
+                      <InfoIcon mr="3" mt="1"/>
+                      <Text
+                        p="0"
+                        m="0"
+                        fontSize="12"
+                        fontStyle="italic"
+                        overflowWrap="break-word"
+                      >
+                        Restrict the bounty to allow claiming only by certain users or roles
+                      </Text>
+                    </Flex>
+                  </FormLabel>
+                  <Select id='gatedTo' {...register('gatedTo', {
+                    validate: v => {
+                      if (gated) {
+                       return (v && gated) ? true : required
+                      } else {
+                        return true 
+                      }
+                   }})}>
+                    {currencies.map(c => <option key={c}>{c.toUpperCase()}</option>)}
+                  </Select>
+                  <FormErrorMessage>{errors.gatedTo?.message}</FormErrorMessage>
+                </ExpansionField> 
+          </FormControl>
+        <Button
+          mt="5"
+          w="full" 
+          type="submit"
+        >
+          Preview
+        </Button>
+      </form>
     </Layout>
   )
 }
