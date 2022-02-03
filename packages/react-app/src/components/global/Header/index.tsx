60d0eefc81109439f0bedf64f107397289bfbfca
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2022-02-03 01:16:25 -0800

    add bank logo

--- packages/react-app/src/components/global/Header/index.tsx
+++ packages/react-app/src/components/global/Header/index.tsx
@@ -1,40 +1,49 @@
-import React, { useContext, useState } from 'react';
-import { Flex } from '@chakra-ui/react';
-import Logo from './Logo';
-import { MenuLinks, MenuToggle } from './Menu';
-import { CustomerContext } from '../../../context/CustomerContext';
+import React, { useContext, useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import { MenuLinks, MenuToggle } from "./Menu";
+import { CustomerContext } from "../../../context/CustomerContext";
+import Image from "next/image";
 
 const NavBarContainer: React.FC = (props): JSX.Element => (
-	<Flex
-		as="nav"
-		align="center"
-		justify="space-between"
-		wrap="wrap"
-		w="100%"
-		p={8}
-		{...props}
-	>
-		{props.children}
-	</Flex>
+  <Flex
+    as="nav"
+    align="center"
+    justify="space-between"
+    wrap="wrap"
+    w="100%"
+    p={8}
+    {...props}
+  >
+    {props.children}
+  </Flex>
 );
 
-const NavBar: React.FC = (props): JSX.Element => {
-	const [isOpen, setIsOpen] = useState(false);
-	const toggle = () => setIsOpen(!isOpen);
-	const { customer } = useContext(CustomerContext);
-	return (
-		<NavBarContainer {...props}>
-			{ !isOpen ?
-				<Logo
-					alt={`${customer?.customerName ?? 'DAO'} Logo`}
-					img={customer?.customization?.logo ?? './logo.png'}/>
-				: null
-			}
-			<MenuToggle toggle={toggle} isOpen={isOpen} />
-			<MenuLinks isOpen={isOpen} />
-		</NavBarContainer>
-	);
+const myLoader = ({ src, width, quality }) => {
+  return `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.2p9BfZBVR9JDnNyMsTqXngHaHa%26pid%3DApi&f=1${src}?w=${width}&q=${
+    quality || 75
+  }`;
 };
 
+const NavBar: React.FC = (props): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const { customer } = useContext(CustomerContext);
+  return (
+    <NavBarContainer {...props}>
+      {!isOpen ? (
+        <Image
+          loader={myLoader}
+          src="src"
+          alt={`${customer?.customerName ?? "DAO"} Logo`}
+          width={50}
+          height={50}
+          quality={100}
+        />
+      ) : null}
+      <MenuToggle toggle={toggle} isOpen={isOpen} />
+      <MenuLinks isOpen={isOpen} />
+    </NavBarContainer>
+  );
+};
 
 export default NavBar;
