715db134494e56356f2874831552a453398a69e1
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2022-02-07 00:19:32 -0800

    fixed-logo-edit-endpoint

--- packages/react-app/src/components/global/Header/index.tsx
+++ packages/react-app/src/components/global/Header/index.tsx
@@ -1,8 +1,8 @@
+import Image from "next/image";
 import React, { useContext, useState } from "react";
 import { Flex } from "@chakra-ui/react";
 import { MenuLinks, MenuToggle } from "./Menu";
 import { CustomerContext } from "../../../context/CustomerContext";
-import Image from "next/image";
 
 const NavBarContainer: React.FC = (props): JSX.Element => (
   <Flex
@@ -18,12 +18,6 @@ const NavBarContainer: React.FC = (props): JSX.Element => (
   </Flex>
 );
 
-const myLoader = ({ src, width, quality }) => {
-  return `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.2p9BfZBVR9JDnNyMsTqXngHaHa%26pid%3DApi&f=1${src}?w=${width}&q=${
-    quality || 75
-  }`;
-};
-
 const NavBar: React.FC = (props): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
@@ -32,12 +26,10 @@ const NavBar: React.FC = (props): JSX.Element => {
     <NavBarContainer {...props}>
       {!isOpen ? (
         <Image
-          loader={myLoader}
-          src="src"
-          alt={`${customer?.customerName ?? "DAO"} Logo`}
-          width={50}
+          src={customer?.customization?.logo ?? "/logo.png"}
           height={50}
-          quality={100}
+          width={50}
+          alt={`${customer?.customerName ?? "DAO"} Logo`}
         />
       ) : null}
       <MenuToggle toggle={toggle} isOpen={isOpen} />
--- packages/react-app/src/constants/Bankless.ts
+++ packages/react-app/src/constants/Bankless.ts
@@ -5,6 +5,6 @@ import { CustomerProps } from '../models/Customer';
 export const BANKLESS: CustomerProps = {
 	customerName: 'BanklessDAO',
 	customer_id: '834499078434979890',
-	customization: { logo: './logo.png' },
+	customization: { logo: '/logo.png' },
 	bountyChannel: '850402063741091880',
 };
