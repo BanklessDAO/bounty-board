bfda8737e300de954de1d61e52f33674b03c9266
your-user-name <your.email@example.com>
2022-02-10 18:28:12 -0700

    Moved menu items into their own component called MenuLinks for easy reading.  Added useDiscourse method from chakraUI to handle the toggling of the mobile menu, mobile menu will be in next commit.  Updated the index file to remove the now uneeded isOpen state var.  Added some styling rules to the buttons and links in the menu.

--- packages/react-app/src/components/global/Header/index.tsx
+++ packages/react-app/src/components/global/Header/index.tsx
@@ -1,40 +1,34 @@
-import React, { useContext, useState } from 'react';
-import { Flex } from '@chakra-ui/react';
-import Logo from './Logo';
-import { MenuLinks, MenuToggle } from './Menu';
-import { CustomerContext } from '../../../context/CustomerContext';
+import React, { useContext } from "react";
+import { Flex } from "@chakra-ui/react";
+import Logo from "./Logo";
+import { Menu } from "./Menu";
+import { CustomerContext } from "../../../context/CustomerContext";
 
 const NavBarContainer: React.FC = (props): JSX.Element => (
-	<Flex
-		as="nav"
-		align="center"
-		justify="space-between"
-		wrap="wrap"
-		w="100%"
-		p={8}
-		{...props}
-	>
-		{props.children}
-	</Flex>
+  <Flex
+    as="nav"
+    align="center"
+    justify="space-between"
+    wrap="wrap"
+    w="100%"
+    p={8}
+    {...props}
+  >
+    {props.children}
+  </Flex>
 );
 
 const NavBar: React.FC = (props): JSX.Element => {
-	const [isOpen, setIsOpen] = useState(false);
-	const toggle = () => setIsOpen(!isOpen);
-	const { customer } = useContext(CustomerContext);
-	return (
-		<NavBarContainer {...props}>
-			{ !isOpen ?
-				<Logo
-					alt={`${customer?.customerName ?? 'DAO'} Logo`}
-					img={customer?.customization?.logo ?? './logo.png'}/>
-				: null
-			}
-			<MenuToggle toggle={toggle} isOpen={isOpen} />
-			<MenuLinks isOpen={isOpen} />
-		</NavBarContainer>
-	);
+  const { customer } = useContext(CustomerContext);
+  return (
+    <NavBarContainer {...props}>
+      <Logo
+        alt={`${customer?.customerName ?? "DAO"} Logo`}
+        img={customer?.customization?.logo ?? "./logo.png"}
+      />
+      <Menu />
+    </NavBarContainer>
+  );
 };
-
 
 export default NavBar;
