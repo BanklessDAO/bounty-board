cf1df136d327d72b0026849ad28e9454097b97bc
Zara-Gma <43872052+Zara-Gma@users.noreply.github.com>
2022-02-22 20:24:31 -0800

    conflict-resolved

--- packages/react-app/src/constants/Bankless.ts
+++ packages/react-app/src/constants/Bankless.ts
@@ -1,10 +1,17 @@
-import { CustomerProps } from '../models/Customer';
+import { CustomerProps } from "../models/Customer";
 /**
  * Fallback data for the bountyboard, that can be rendered on load.
  */
 export const BANKLESS: CustomerProps = {
-	customerName: 'BanklessDAO',
-	customer_id: '834499078434979890',
-	customization: { logo: '/logo.png' },
-	bountyChannel: '850402063741091880',
+  customerName: "BanklessDAO",
+  customerId: "834499078434979890",
+  customization: { logo: "./logo.png" },
+  bountyChannel: "850402063741091880",
+};
+
+export const BBBS = {
+  customerName: "BBBS",
+  customization: { logo: "./logo.png" },
+  // bountyChannel: '850402063741091880',
+  customerId: "905250069463326740",
 };
