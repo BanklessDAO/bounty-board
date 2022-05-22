ae098b0d66816821f409dd8846d078b6641fbfd7
Tim Escher <tim@eschers.com>
2022-05-22 07:54:26 -0800

    Interim save

--- packages/react-app/src/components/pages/Bounties/Bounty/index.tsx
+++ packages/react-app/src/components/pages/Bounties/Bounty/index.tsx
@@ -7,28 +7,44 @@ import {
 	Box,
 	Checkbox,
 	Flex,
-	Grid,
-	GridItem,
-	Heading,
 	Tag,
 	TagLabel,
-	Text,
 	HStack,
+	Heading,
+	useDisclosure,
+	Modal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalCloseButton,
 } from '@chakra-ui/react';
 import { BountyCollection } from '@app/models/Bounty';
-import BountyClaim from './claim';
-import BountySubmit from './submit';
-import { BountyEditButton } from './edit';
+// import BountyClaim from './claim';
+// import BountySubmit from './submit';
+// import { BountyEditButton } from './edit';
 import UserAvatar from '@app/components/parts/UserAvatar';
+import PAID_STATUS from '@app/constants/paidStatus';
 
 type SetState<T extends any> = (arg: T) => void;
 
-const Status = ({ bounty }: { bounty: BountyCollection }): JSX.Element => (
+interface StatusProps {
+	bounty: BountyCollection,
+	withAvatar?: boolean,
+}
+const Status = ({ bounty, withAvatar }: StatusProps): JSX.Element => (
 	<Tag my={0} size="lg" key="lg" variant="outline" colorScheme={bounty.status}>
+		{ withAvatar &&
 		<Box ml={-1} mr={2} >
 			<UserAvatar userId={bounty.claimedBy?.discordId} size='xs' />
-		</Box>
+		</Box> }
 		<TagLabel>{bounty.status.replace('-', ' ')}</TagLabel>
+	</Tag>
+);
+
+const PaidStatus = ({ bounty }: {bounty: BountyCollection}): JSX.Element => (
+	<Tag my={0} size="lg" key="lg" variant="outline" colorScheme={bounty.paidStatus ? bounty.paidStatus : PAID_STATUS.UNPAID}>
+		<TagLabel>{bounty.paidStatus ? bounty.paidStatus : PAID_STATUS.UNPAID}</TagLabel>
 	</Tag>
 );
 
@@ -62,13 +78,13 @@ const BountySelect = ({ selectedBounties, setSelectedBounties, bountyId }: {
 	);
 };
 
-const DiscordStub = ({ name }: { name: string }): JSX.Element => (
-	<Flex my={2} align="center" gridGap={3}>
-		<Text fontSize="md" color="steelblue">
-			@{name}
-		</Text>
-	</Flex>
-);
+// const DiscordStub = ({ name }: { name: string }): JSX.Element => (
+// 	<Flex my={2} align="center" gridGap={3}>
+// 		<Text fontSize="md" color="steelblue">
+// 			@{name}
+// 		</Text>
+// 	</Flex>
+// );
 
 const calculateReward = (_reward: BountyCollection['reward']): string => {
 	return `${_reward.amount ?? 0} ${_reward.currency}`;
@@ -108,78 +124,134 @@ export const BountySummary = ({ bounty }: {bounty: BountyCollection}): JSX.Eleme
 				</Box>
 				<Spacer />
 				<Box mb={2}>
-					{bounty.status && <Status bounty={bounty} />}
+					{bounty.status && <Status bounty={bounty} withAvatar={true}/>}
 				</Box>
 			</Flex>
 		</Flex>
 	);
 };
 
-const BountyDetails = ({ bounty }: { bounty: BountyCollection }): JSX.Element => {
-	const {
-		_id,
-		description,
-		criteria,
-		createdBy,
-		claimedBy,
-		createdAt,
-		status,
-		dueAt,
-	} = bounty;
+const BountyModal = ({ bounty, isOpen, onClose }: { bounty: BountyCollection, isOpen: boolean, onClose: () => void }): JSX.Element => {
 	return (
-		<Grid gap={6}>
-			{_id &&
-				<GridItem>
-					<Heading size="sm">Bounty ID</Heading>
-					<Text>{_id}</Text>
-				</GridItem>
-			}
-			<GridItem>
-				<Heading size="sm">Created Date</Heading>
-				<Text>{createdAt.substring(0, 10)}</Text>
-			</GridItem>
-			<GridItem>
-				<Heading size="sm">Description</Heading>
-				<Text>{description}</Text>
-			</GridItem>
-			<GridItem>
-				<Heading size="sm">Done Criteria</Heading>
-				<Text>{criteria}</Text>
-			</GridItem>
-			{dueAt &&
-				<GridItem>
-					<Heading size="sm">Deadline</Heading>
-					<Text>{new Date(dueAt).toDateString()}</Text>
-				</GridItem>
-			}
-			{
-				createdBy
-					? <GridItem>
-						<Heading size="sm">Requested By</Heading>
-						<DiscordStub name={createdBy.discordHandle as string} />
-					</GridItem>
-					: null
-			}
-			<GridItem>
-				<Flex>{
-					status && status.toLowerCase() === 'draft'
-						? <BountySubmit bounty={bounty} />
-						: claimedBy
-							? (
-								<Flex flexDirection={'column'} justifyContent="start">
-									<Heading size="sm" my='0'>Claimed By</Heading>
-									<DiscordStub name={claimedBy.discordHandle ?? 'Unknown'} />
-								</Flex>
-							)
-							: status.toLowerCase() !== 'deleted' && <BountyClaim bounty={bounty} />
-				}
-				<Box ml="5">
-					<BountyEditButton bounty={bounty} />
-				</Box>
-				</Flex>
-			</GridItem>
-		</Grid>
+		<Modal isOpen={isOpen} onClose={onClose} >
+		  <ModalOverlay />
+		  <ModalContent maxW={'700px'} >
+				<ModalHeader>Modal Title</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>
+			  <BountyDetails bounty={bounty} />
+				</ModalBody>
+		  </ModalContent>
+		</Modal>
 	);
+};
+
+const BountyDetails = ({ bounty }: { bounty: BountyCollection }): JSX.Element => {
+
+	return (
+		<Flex flexWrap="nowrap" width="100%" pl="2" pr="2" >
+			<Flex flexWrap="wrap" alignItems="center" width="100%">
+				<Box width="100px" display="flex" justifyContent="flex-end" pr="2">
+					status
+				</Box>
+				<Box pr="2">
+					<Status bounty={bounty} />
+				</Box>
+				<Box pr="2">
+					<PaidStatus bounty={bounty} />
+				</Box>
+				<Spacer />
+				<Box justifyContent="right">
+					{bounty.reward && (
+						<Heading size="md" mb={0}>
+							{calculateReward(bounty.reward)}
+						</Heading>
+					)}
+				</Box>
+			</Flex>
+		</Flex>
+	);
+	// const {
+	// 	_id,
+	// 	description,
+	// 	criteria,
+	// 	createdBy,
+	// 	claimedBy,
+	// 	createdAt,
+	// 	status,
+	// 	dueAt,
+	// } = bounty;
+	// return (
+	// 	<Grid gap={6} width={{ base: '95vw', lg: '700px' }}>
+	// 		{_id &&
+	// 			<GridItem>
+	// 				<Heading size="sm">Bounty ID</Heading>
+	// 				<Text>{_id}</Text>
+	// 			</GridItem>
+	// 		}
+	// 		<GridItem>
+	// 			<Text>status</Text>
+	// 			<Status bounty={bounty} />
+	// 			<Box
+	// 				width={{ base: '100%', md: '30%' }}
+	// 				textAlign={{ base: 'left', md: 'right' }}
+	// 				mt={{ base: 0, md: 2 }}
+	// 				ml="auto"
+	// 			>
+	// 				{bounty.reward && (
+	// 					<Heading mb={4} size="md">
+	// 						{calculateReward(bounty.reward)}
+	// 					</Heading>
+	// 				)}
+	// 			</Box>
+
+	// 		</GridItem>
+	// 		<GridItem>
+	// 			<Heading size="sm">Created Date</Heading>
+	// 			<Text>{createdAt.substring(0, 10)}</Text>
+	// 		</GridItem>
+	// 		<GridItem>
+	// 			<Heading size="sm">Description</Heading>
+	// 			<Text>{description}</Text>
+	// 		</GridItem>
+	// 		<GridItem>
+	// 			<Heading size="sm">Done Criteria</Heading>
+	// 			<Text>{criteria}</Text>
+	// 		</GridItem>
+	// 		{dueAt &&
+	// 			<GridItem>
+	// 				<Heading size="sm">Deadline</Heading>
+	// 				<Text>{new Date(dueAt).toDateString()}</Text>
+	// 			</GridItem>
+	// 		}
+	// 		{
+	// 			createdBy
+	// 				? <GridItem>
+	// 					<Heading size="sm">Requested By</Heading>
+	// 					<DiscordStub name={createdBy.discordHandle as string} />
+	// 				</GridItem>
+	// 				: null
+	// 		}
+	// 		<GridItem>
+	// 			<Flex>{
+	// 				status && status.toLowerCase() === 'draft'
+	// 					? <BountySubmit bounty={bounty} />
+	// 					: claimedBy
+	// 						? (
+	// 							<Flex flexDirection={'column'} justifyContent="start">
+	// 								<Heading size="sm" my='0'>Claimed By</Heading>
+	// 								<DiscordStub name={claimedBy.discordHandle ?? 'Unknown'} />
+	// 							</Flex>
+	// 						)
+	// 						: status.toLowerCase() !== 'deleted' && <BountyClaim bounty={bounty} />
+	// 			}
+	// 			<Box ml="5">
+	// 				<BountyEditButton bounty={bounty} />
+	// 			</Box>
+	// 			</Flex>
+	// 		</GridItem>
+	// 	</Grid>
+	// );
 };
 
 export const BountyCard = ({ bounty }: { bounty: BountyCollection }): JSX.Element => {
@@ -212,13 +284,22 @@ export const AccordionBountyItem = ({ bounty, selectedBounties, setSelectedBount
 	</AccordionItem>
 );
 
-export const BountyItem = ({ bounty, selectedBounties, setSelectedBounties }: { bounty: BountyCollection, selectedBounties: string[], setSelectedBounties: SetState<string[]> }): JSX.Element => (
-	<Box w='100%' borderWidth={3} borderRadius={10} mb={1}>
-		<HStack>
-			<BountySelect bountyId={bounty._id} selectedBounties={selectedBounties} setSelectedBounties={setSelectedBounties} />
-			<Box w='100%' pb={2} pt={0}>
-				<BountySummary bounty={bounty} />
-			</Box>
-		</HStack>
-	</Box>
-);
+export const BountyItem = ({ bounty, selectedBounties, setSelectedBounties }: { bounty: BountyCollection, selectedBounties: string[], setSelectedBounties: SetState<string[]> }): JSX.Element => {
+	const { isOpen: isBountyModalOpen, onOpen: onBountyModalOpen, onClose: onBountyModalClose } = useDisclosure();
+
+	return(
+		<Box w='100%' borderWidth={3} borderRadius={10} mb={1}>
+			<HStack>
+				<BountySelect bountyId={bounty._id} selectedBounties={selectedBounties} setSelectedBounties={setSelectedBounties} />
+				<Box w='100%' pb={2} pt={0} onClick={onBountyModalOpen}>
+					<BountySummary bounty={bounty} />
+				</Box>
+				<BountyModal
+					isOpen={isBountyModalOpen}
+					onClose={onBountyModalClose}
+					bounty={bounty}
+				/>
+			</HStack>
+		</Box>
+	);
+};
